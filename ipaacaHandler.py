f480b115138e3af8f6b795be2f6ab4614ebf64d7
Jan Pöppel <jpoeppel@techfak.uni-bielefeld.de>
2016-08-11 16:12:32 +0200

    workaround for removing categories. The callback will now always be registered after a categorie has been removed. A category can now be added multiple times (for the ipaacaPlot) and it will only be removed after all occurances have been removed

--- ipaacaHandler.py
+++ ipaacaHandler.py
@@ -33,94 +33,90 @@ class IpaacaHandler(object):
       self.outBuffer = ipaaca.OutputBuffer(self.component)
       # input buffer with a list of categories that this module is listening for
       if len(self.inputCategories) > 0:
-        self.inBuffer = ipaaca.InputBuffer(self.component, self.inputCategories)
-        # register this module's update handler, so that received IUs can be processed
-        if self.callback != None:
-          self.inBuffer.register_handler(self.callback)      
+          self.inBuffer = ipaaca.InputBuffer(self.component, self.inputCategories)
+          # register this module's update handler, so that received IUs can be processed
+          if self.callback != None:
+              self.inBuffer.register_handler(self.callback)      
       else:
-        self.inBuffer = None
+          self.inBuffer = None
         
     def publish(self, category, payload):
-      """
-        Publishing method that sends a message of given kategory and payload.
+        """
+            Publishing method that sends a message of given kategory and payload.
         
-        @param category: The category this message should have
-        @param payload: A dictionary containing the payload as key-value pairs
-      """
-      msg = ipaaca.Message(category)
-      msg.payload = payload
-      self.outBuffer.add(msg)
+            @param category: The category this message should have
+            @param payload: A dictionary containing the payload as key-value pairs
+        """
+        msg = ipaaca.Message(category)
+        msg.payload = payload
+        self.outBuffer.add(msg)
       
     def setCallback(self, callback):
-      """
-        Function to set the callback function that is to be triggered by the inBuffer.
+        """
+            Function to set the callback function that is to be triggered by the inBuffer.
         
-        @param callback: Function handle that is used for the callbacks.
-      """
-      self.callback = callback
-      if len(self.inputCategories) > 0:
-        if self.callback != None:
-          self.inBuffer.register_handler(self.callback)  
+            @param callback: Function handle that is used for the callbacks.
+        """
+        self.callback = callback
+        if len(self.inputCategories) > 0:
+            if self.callback != None:
+                self.inBuffer.register_handler(self.callback)  
           
         
     def addInputCategory(self, category):
-      """
-        Function that allows to add a category to the interesting categories of the 
-        inBuffer.
+        """
+            Function that allows to add a category to the interesting categories of the 
+            inBuffer.
         
-        @param category: The category name that is to be added.
-      """
-      
-      print "adding category", category
-      if category not in self.inputCategories:
+            @param category: The category name that is to be added.
+        """
+        print "adding category", category
+#        if category not in self.inputCategories:
         self.inputCategories.append(category)
-      if len(self.inputCategories) > 0:
-        if self.inBuffer != None:
-          print "adding single"
-          self.inBuffer.add_category_interests(category)
-        else:
-          print "creating new"  
-          self.inBuffer = ipaaca.InputBuffer(self.component, self.inputCategories)
+        if len(self.inputCategories) > 0:
+            if self.inBuffer != None:
+                self.inBuffer.add_category_interests(category)
+            else:
+                self.inBuffer = ipaaca.InputBuffer(self.component, self.inputCategories)
         # register this module's update handler, so that received IUs can be processed
         if self.callback != None and not self.callbackSet:
-          self.inBuffer.register_handler(self.callback)  
-          self.callbackSet =  True
+            self.inBuffer.register_handler(self.callback)  
+            self.callbackSet =  True
           
     def removeInputCategory(self, category):
-      """
-        Function to remove a category from the list of interesting categories the
-        inputBuffer listens to.
+        """
+            Function to remove a category from the list of interesting categories the
+            inputBuffer listens to.
         
-        Manipulates lower level variables since there is no direct access.
-        
-        @param category: The category name that is to be removed.
-      """
-      print "Removing category", category
-      if category in self.inputCategories:
-        self.inputCategories.remove(category)
-#        del self.inBuffer._listener_store[category] # Not needed anymore since remove
-#        self.inBuffer._category_interests.remove(category)
-        if not category in self.inputCategories:
-            self.inBuffer.remove_category_interests(category)
-      if len(self.inputCategories) == 0:
-        self.inBuffer = None
+            Manipulates lower level variables since there is no direct access.
+            
+            @param category: The category name that is to be removed.
+        """
+        print "Removing category", category
+        if category in self.inputCategories:
+            self.inputCategories.remove(category)
+            if not category in self.inputCategories:
+                self.inBuffer.remove_category_interests(category)
+    #            del self.inBuffer._listener_store[category] # Not needed anymore since remove
+    #            self.inBuffer._category_interests.remove(category)
+                self.callbackSet = False
+        if len(self.inputCategories) == 0:
+            self.inBuffer = None
 
 
 if __name__ == "__main__":
+    import sys
   
-  import sys
-  
-  handler = IpaacaHandler("BasicIpaacaDummy", [], None)
-  
-  print "Input Category:"
-  cat = sys.stdin.readline().replace('\n','')
-  print "Inserted Category: ", cat
-  try:
-    print "Input 'key:value' to send an ipaaca message containing the data as payload"
-    while True:
-      payload = sys.stdin.readline().replace('\n','').split(':')
-      print "Sending: ", payload
-      handler.publish(cat, {payload[0].strip():payload[1].strip()})
-  except:
-    sys.exit(0)
+    handler = IpaacaHandler("BasicIpaacaDummy", [], None)
+    print "Input Category:"
+    cat = sys.stdin.readline().replace('\n','')
+    print "Inserted Category: ", cat
+    try:
+        print "Input 'key:value' to send an ipaaca message containing the data as payload"
+        while True:
+            payload = sys.stdin.readline().replace('\n','').split(':')
+            print "Sending: ", payload
+            handler.publish(cat, {payload[0].strip():payload[1].strip()})
+    except:
+        sys.exit(0)
   
